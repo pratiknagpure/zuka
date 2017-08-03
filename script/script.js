4e0af3ddbe42580305c1be5a47119b7e9d35f2e0
pratiknagpure <pratiknagpure@hotmail.com>
2017-08-03 10:09:55 +0530

    about page added

--- script/script.js
+++ script/script.js
@@ -2,4 +2,35 @@ $(document).ready(function() {
   $('#sidebar li').click(function() {
     $(this).addClass('active').siblings().removeClass('active');
   });
+
+// for quantity increment of product
+  var quantitiy=0;
+
+     $('.quantity-right-plus').click(function(e){
+
+          // Stop acting like a button
+          e.preventDefault();
+          // Get the field name
+          var quantity = parseInt($('#quantity').val());
+
+          // If is not undefined
+
+              $('#quantity').val(quantity + 1);
+
+      });
+
+       $('.quantity-left-minus').click(function(e){
+          // Stop acting like a button
+          e.preventDefault();
+          // Get the field name
+          var quantity = parseInt($('#quantity').val());
+
+          // If is not undefined
+
+              // Increment
+              if(quantity>0){
+              $('#quantity').val(quantity - 1);
+              }
+      });
+
 });
